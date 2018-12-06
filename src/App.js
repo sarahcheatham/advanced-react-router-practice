d9eb92313baa692d78e0629270669ff78907b099
sarahcheatham <sarah.cheatham14@gmail.com>
2018-12-05 21:33:47 -0600

    router for comments and dashboard working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Comments from "./components/Comments";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
 
+
 function App() {
   return (
-    <div> 
-      <div id="wrapper">
-        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          <TopNav />
-          <SideNav />
-        </nav>
-        <div style={{backgroundColor: "white"}}>
-          {/* PUT YOUR ROUTES HERE */}
+    <BrowserRouter>
+      <div> 
+        <div id="wrapper">
+          <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <TopNav />
+            <SideNav />
+          </nav>
+          <div style={{backgroundColor: "white"}}>
+          <Switch>
+            <Route path="/comments" component={Comments}/>
+            <Route path="/" component={Dashboard}/>
+            {/* PUT YOUR ROUTES HERE */}
+          </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
 
   );
 }
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,16 +1,17 @@
 import React from "react";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function SideNav() {
   return (
     <div className="collapse navbar-collapse navbar-ex1-collapse">
       <ul className="nav navbar-nav side-nav">
         <li className="active">
-          {/*
+          <Link to="/comments"> <i className="fa fa-fw fa-dashboard" />
+            Comments
+          </Link>
           <Link to="/"> <i className="fa fa-fw fa-dashboard" />
             Dashboard 
           </Link>
-          */}
         </li>
         <li>
           <a href="charts.html">
